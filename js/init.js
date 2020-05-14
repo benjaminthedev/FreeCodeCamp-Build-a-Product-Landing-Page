04e99fd6d9893fcee8a48b80143aa3b8af2f01d3
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2020-05-14 11:37:43 +0100

    Guitar lessons hurts my hand....

--- js/init.js
+++ js/init.js
@@ -1,8 +1,20 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+//Link Click Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
